c3329485656efccfabf4920c873731747f15f2f0
maxdhaese <maxdhaese@gmail.com>
2019-06-17 11:46:19 +0200

    commit

---
+++ src/forestbook_app/ForestBookApp.java
@@ -0,0 +1,14 @@
+package forestbook_app;
+
+import entities.plant_entities.Weed;
+import service.ForestNotebook;
+
+public class ForestBookApp {
+    public static void main(String[] args) {
+
+        ForestNotebook forestNotebook = new ForestNotebook();
+
+        Weed 
+
+    }
+}
--- src/service/ForestNotebook.java
+++ src/service/ForestNotebook.java
@@ -6,6 +6,8 @@ import entities.animal_entities.Herbivore;
 import entities.animal_entities.Omnivore;
 import entities.plant_entities.Plant;
 
+import java.sql.SQLOutput;
+import java.util.Comparator;
 import java.util.List;
 
 public class ForestNotebook {
@@ -16,6 +18,102 @@ public class ForestNotebook {
     private int animalCount;
     private List<Animal> animals;
     private List<Plant> plants;
+
+    public ForestNotebook() {
+    }
+
+    public List<Carnivore> getCarnivores() {
+        return carnivores;
+    }
+
+    public void setCarnivores(List<Carnivore> carnivores) {
+        this.carnivores = carnivores;
+    }
+
+    public List<Omnivore> getOmnivores() {
+        return omnivores;
+    }
+
+    public void setOmnivores(List<Omnivore> omnivores) {
+        this.omnivores = omnivores;
+    }
+
+    public List<Herbivore> getHerbivores() {
+        return herbivores;
+    }
+
+    public void setHerbivores(List<Herbivore> herbivores) {
+        this.herbivores = herbivores;
+    }
+
+    //return the size of List of plants = the amount of plants
+    public int getPlantCount() {
+        return plants.size();
+    }
+
+    //return the size of List of animals = the amount of animals
+    public int getAnimalCount() {
+        return animals.size();
+    }
+
+    //add animals to the right list by checking with instanceof. Whenever an animal is added, the animal count goes up.
+    public void addAnimal(Animal animal){
+        if (animal != null){
+            animals.add(animal);
+            animalCount = animals.size();
+            if (animal instanceof  Carnivore){
+                carnivores.add((Carnivore) animal);
+            }else if (animal instanceof Herbivore){
+                herbivores.add((Herbivore) animal);
+            }else if (animal instanceof Omnivore){
+                omnivores.add((Omnivore) animal);
+            }
+
+        }
+
+    }
+
+
+    //add a plant to plant list and a plant to plantCount
+    public void addPlant(Plant plant){
+        if (plant != null){
+            if (this.plants.indexOf(plant) == -1){
+                this.plants.add(plant);
+                plantCount = plants.size();
+            }
+        }
+
+    }
+    //iterate through the lists and print them
+    public void printNoteBook(){
+        System.out.println("List of Animals:");
+        animals.forEach(System.out::println);
+        System.out.println("List of Plants:");
+        plants.forEach(System.out::println);
+
+    }
+
+    public void sortAnimalsByName(){
+        this.animals.sort(Comparator.comparing(Animal::getName));
+
+    }
+
+    public void sortPlantsByName(){
+        this.plants.sort(Comparator.comparing(Plant::getName));
+
+    }
+
+    public void sortAnimalsByHeight(){
+        this.animals.sort(Comparator.comparing(Animal::getHeight));
+    }
+
+    public void sortPlantsByHeight(){
+        this.plants.sort(Comparator.comparing(Plant::getHeight));
+
+    }
+
+
+
 
 
 }
